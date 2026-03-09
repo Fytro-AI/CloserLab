8b454838be67dcfd9853e069e681fe04f091847e
CloserLab <fytroai@gmail.com>
2026-03-09 08:50:30 +0200

    Bug fixes and performance updates

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider, useAuth } from "@/hooks/useAuth";
-import { Analytics } from "@vercel/analytics/next"
 import Navbar from "@/components/Navbar";
 import Landing from "./pages/Landing";
 import Auth from "./pages/Auth";
@@ -55,7 +54,6 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <Analytics />
       <BrowserRouter>
         <AuthProvider>
           <AppRoutes />
